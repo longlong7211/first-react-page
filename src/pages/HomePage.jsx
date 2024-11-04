3bc6e534f7505a8bc54f9c0ca0bc98a447bbf039
Hồ Ngọc Hoàng Long <51793029+longlong7211@users.noreply.github.com>
2024-11-04 09:25:59 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import CtdtCard from '../components/ctdtCard'
 export default function HomePage() {
   return <>
     <div className="container d-flex flex-column align-items-center">
-      code today
+      code today update
       <div className="d-flex mt-5">
         <CtdtCard count={100} title={"Lớp"} />
         <CtdtCard count={100} title={"Lớp"} />
