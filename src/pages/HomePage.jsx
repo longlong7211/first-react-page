95e128ce8c1188f5aaff8d32b8a2a52573a83ddd
Hoàng Long <longhnh@lhu.edu.vn>
2024-11-04 09:16:10 +0700

    update code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,11 @@ import CtdtCard from '../components/ctdtCard'
 export default function HomePage() {
   return <>
     <div className="container d-flex flex-column align-items-center">
-      <h1>TUYỂN SINH</h1>
-      <div>Đại học Lạc Hồng là trường đào tạo đa ngành, đa lĩnh vực</div>
-      <div className="d-flex">
-        <CtdtCard icon={<></>} count={1000} title="Chuong trinh dao tao tien si" />
-        <CtdtCard icon={<></>} count={200} title="Chuong trinh dao tao thac si" />
-        <CtdtCard icon={<></>} count={10} title="Chuong trinh dao tao dai hoc" />
-        <CtdtCard icon={<></>} count={5} title="Chuong trinh dao tao thac si" />
+      code today
+      <div className="d-flex mt-5">
+        <CtdtCard count={100} title={"Lớp"} />
+        <CtdtCard count={100} title={"Lớp"} />
+        <CtdtCard count={100} title={"Lớp"} />
       </div>
     </div>
   </>
